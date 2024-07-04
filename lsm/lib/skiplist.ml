b83dafb24c464516d4b9afae6593324f902ca2a5
Aarushi <anu@Aarushis-MacBook-Pro.local>
2024-07-04 20:53:52 +0530

    Proper ADT

--- lsm/lib/skiplist.ml
+++ lsm/lib/skiplist.ml
@@ -1,18 +1,29 @@
 
-(* open Stdlib *)
+open Stdlib
+
+let () = Random.self_init()
 
 type 'v values =  Int
 type 'k keys =  Int
 
-type ('k, 'v) node =
-  | NodeKey of ('k) keys
-  | NodeValue of ('v) values
-  | NodeSibling of ('k, 'v) t
-  | NodeChild of ('k, 'v) t
-and ('k, 'v) t =
-  | Node of ('k, 'v) node
-  | Empty
 
-type ('k, 'v) head =
-  | NodeSibling of ('k, 'v) t
-  | NodeChild of ('k, 'v) t
+type ('k, 'v) skiplist = Empty | Head of ('k, 'v) head |  Node of ('k, 'v) node
+and  ('k, 'v)  node =
+  { keys : 'k ;
+    values : 'v;
+    nodesibling :  ('k, 'v) skiplist;
+    nodechild :  ('k, 'v) skiplist
+  }
+and ('k, 'v) head =
+   { sibling :  ('k, 'v) skiplist ;
+     child :  ('k, 'v) skiplist
+   }
+type ('k, 'v) skip_list  =
+  { skpseed : int ;  (*Seed *)
+    skpinternal :  ('k, 'v) head }
+
+
+let empty_check (skp : ('k, 'v) skip_list) : bool =
+  match skp.skpinternal with
+  | { sibling = Empty; child = Empty } -> true
+  | _ -> false
